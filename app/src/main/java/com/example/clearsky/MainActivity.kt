315f4c60aef3f62732dbe943d2e64d13f712406c
Tom Laplace <tomlaplace0902@gmail.com>
2023-03-27 11:41:28 +0200

    feat/ recherche ville

--- app/src/main/java/com/example/clearsky/MainActivity.kt
+++ app/src/main/java/com/example/clearsky/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.clearsky
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textViewCity: TextView
     private lateinit var textViewDescription: TextView
     private lateinit var textViewTemperature: TextView
+    private lateinit var editTextCitySearch: EditText
+    private lateinit var buttonSearch: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +27,21 @@ class MainActivity : AppCompatActivity() {
         textViewDescription = findViewById(R.id.textViewDescription)
         textViewTemperature = findViewById(R.id.textViewTemperature)
 
+
         viewModel = ViewModelProvider(this).get(WeatherViewModel::class.java)
 
+        val editTextCitySearch = findViewById<EditText>(R.id.editTextCitySearch)
+        val buttonSearch = findViewById<Button>(R.id.buttonSearch)
         val apiKey = "a2317e7fe800f51f1e2ddebed66a9be8"
-        viewModel.getWeather("Bordeaux", apiKey)
+
+        buttonSearch.setOnClickListener {
+            val city = editTextCitySearch.text.toString().trim()
+            if(city.isNotEmpty()) {
+                viewModel.getWeather(city, apiKey)
+            } else {
+                Toast.makeText(this, "Veuillez entrer un nom valide", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         viewModel.weather.observe(this, Observer { weatherResponse ->
             textViewCity.text = weatherResponse.name
